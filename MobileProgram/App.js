a0b968df11dc5df34058fbf672a1bdf169322022
juusotahtinen <juuso.tahtinen88@hotmail.com>
2021-09-30 12:35:07 +0300

    Update App.js

--- MobileProgram/App.js
+++ MobileProgram/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start JUUUUSOOO!</Text>
+      <Text>Open up App.js to start WWWOOOOOORRRLLDDDD!</Text>
       <StatusBar style="auto" />
     </View>
   );
