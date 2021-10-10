ee3dba1b7c4ef37fb88286dd605f62cf96af3c3b
juusotahtinen <juuso.tahtinen88@hotmail.com>
2021-10-10 17:23:27 +0300

    Loginscreen added

--- MobileProgram/App.js
+++ MobileProgram/App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import LoginScreen from './screens/LoginScreen'
 import InputCriteria from './screens/InputCriteria'
 import ShowCriteria from './screens/ShowCriteria'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler';
+import LoginSCreen from './screens/LoginScreen';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start WWWOOOOOORRRLLDDDD!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Drawer.Navigator>
+    <Drawer.Screen name="Login" component={LoginSCreen} />
+      <Drawer.Screen name="Show Criteria" component={ShowCriteria} />
+      <Drawer.Screen name="Input Criteria" component={InputCriteria} />
+    </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
